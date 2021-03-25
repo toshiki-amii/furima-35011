629a840964680e77222a55961b4f72f68f1fb548
toshiki-amii <mono.non.te@gmail.com>
2021-03-25 12:31:08 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,    presence: true
+  validates :birth,       presence: true
+
+  # 姓名にひらがなカタカナ漢字々ーを設定
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+
+  # 読み仮名にカタカナとーを設定
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :read_family
+    validates :read_name
+  end
+
 end
