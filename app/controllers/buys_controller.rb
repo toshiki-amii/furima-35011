13aab756f574f18407f30dfee0a6b2c2e8809d61
toshiki-amii <80087083+toshiki-amii@users.noreply.github.com>
2021-04-06 14:25:38 +0900

    トークンの生成及び送信、環境変数の設定

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -6,18 +6,31 @@ class BuysController < ApplicationController
   end
 
   def create
+    binding.pry
+    @item = Item.find(params[:item_id])
     @buy_buyer = BuyBuyer.new(buyer_params)
     if @buy_buyer.valid?
+      pay_item
       @buy_buyer.save
-      redirect_to root_path
+      return redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
+
   def buyer_params
-    params.require(:buy_buyer).permit(:postal, :prefecture_id, :city, :address, :building, :phone).merge(buy_id: current_user.id)
+    params.require(:buy_buyer).permit(:postal, :prefecture_id, :city, :address, :building, :phone).merge(item_id: params[:item_id], user_id: current_user, token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buyer_params[:token],
+        currency: 'jpy'
+      )
+  end
+
 end
--- app/models/buy_buyer.rb
+++ app/models/buy_buyer.rb
@@ -1,23 +1,24 @@
 class BuyBuyer
   include ActiveModel::Model
-  attr_accessor :postal, :prefecture_id, :city, :address, :building, :phone, :buy_id, :user_id, :item_id
+  attr_accessor :postal, :prefecture_id, :city, :address, :building, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :address
-    validates :buy_id
     validates :user_id
     validates :item_id
     validates :phone, numericality: { only_integer: true }
   end
 
+  validates :token, presence: { message: 'カード情報を正しく入力してください。' }
+
   validates :prefecture_id, numericality: {other_than: 1 }
   
 
   def save
-    buy = Buy.create(user_id: user.id, item_id: item.id )
-    buyer.create(postal: postal, prefecture_id: prefecture_id, city: city, address: address, building: building, phone: phone, buy_id: buy.id)
+    buy = Buy.create(user_id: user_id, item_id: item_id )
+    Buyer.create(postal: postal, prefecture_id: prefecture_id, city: city, address: address, building: building, phone: phone, buy_id: buy.id)
   end
 
 end
